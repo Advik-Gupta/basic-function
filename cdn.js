c349efef1450bd4fbffe1657c75220c7c19053c3
Advik Gupta <fortknight6901@gmail.com>
2022-03-23 17:04:04 +0530

    added removeFromArray, shuffleArray

--- cdn.js
+++ cdn.js
@@ -1,3 +1,26 @@
 print = function(input) {
 	console.log(input);
 };
+
+removeFromArray = function(arr, value) {
+	return arr.filter(function(ele) {
+		return ele !== value;
+	});
+};
+
+shuffleArray = function(array) {
+	let currentIndex = array.length,
+		randomIndex;
+
+	// While there remain elements to shuffle...
+	while (currentIndex != 0) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[ array[currentIndex], array[randomIndex] ] = [ array[randomIndex], array[currentIndex] ];
+	}
+
+	return array;
+};
--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// Print something (use instead of console.log)
 module.exports.print = function(input) {
 	console.log(input);
+};
+
+// Remove and item from array (arr = array to remove value from, value = value to remove from the array)
+module.exports.removeFromArray = function(arr, value) {
+	return arr.filter(function(ele) {
+		return ele !== value;
+	});
+};
+
+// Shuffle the contents of an array into a random order
+module.exports.shuffleArray = function(array) {
+	let currentIndex = array.length,
+		randomIndex;
+
+	// While there remain elements to shuffle...
+	while (currentIndex != 0) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[ array[currentIndex], array[randomIndex] ] = [ array[randomIndex], array[currentIndex] ];
+	}
+
+	return array;
 };
